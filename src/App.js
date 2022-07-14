4b02a4d815d928be5eb3f1e813e07c7527018265
Sarah Yu <sarahyu0692@gmail.com>
2022-07-14 17:20:45 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+//
+import Station from './components/Station'
 
-function App() {
+export default function App() {
+  const [stations, setStations] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchRadarStations = async () => {
+      let response = await fetch('https://api.weather.gov/radar/stations')
+      
+      if (!response.ok) {
+        throw new Error('Data could not be retrieved.')
+      }
+
+      return response.json()
+    }
+
+    fetchRadarStations()
+      .then(response => {
+        setStations(response.features)
+        console.log(response.features)
+      })
+      .catch(error => setError(error.message))
+
+    return () => setError(null)
+  }, [])
+
+  function getLatLong(coordinates) {
+    return `${coordinates[1]},${coordinates[0]}`
+  }
+
+  function getGoogleMapsLink(coordinates) {
+    const latLong = getLatLong(coordinates)
+
+    return `https://maps.google.com/?q=${latLong}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App text-slate-800'>
+      <main className='container mx-auto py-10 px-5'>
+        <h1 className='text-2xl font-medium'>Radar Stations</h1>
+        <section className='py-4 md:grid md:grid-cols-4 md:gap-4'>
+          <aside className='col-span-1 rounded bg-slate-100'></aside>
+          <div className='col-span-3 sm:grid sm:grid-cols-2 sm:gap-4 lg:grid-cols-3'>
+            {stations.map((station, index) => <Station key={index} station={station} />)}
+          </div>
+        </section>
+      </main>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Station.js
@@ -0,0 +1,41 @@
+export default function Station({ station }) {
+    const {
+        name,
+        id,
+        elevation
+    } = station.properties
+
+    const { coordinates } = station.geometry
+
+    function getLatLong() {
+        return `${coordinates[1]},${coordinates[0]}`
+    }
+
+    function getGoogleMapsLink() {
+        return `https://maps.google.com/?q=${getLatLong(coordinates)}`
+    }
+
+    function renderDetail(property, value) {
+        return (
+            <div className='flex justify-between xl:text-sm'>
+                <span className='font-semibold'>{property}</span>
+                <span>{value}</span>
+            </div>
+        )
+    }
+
+    return (
+        <div className='
+            col-span-1 aspect-square
+            rounded p-4 mb-4 
+            bg-slate-100
+            text-xs
+            flex flex-col
+            md:mb-0'>
+            <div className='mb-auto font-semibold text-base xl:text-lg'>{name}</div>
+            {renderDetail('Identifier', id)}
+            {renderDetail('Altitude', elevation.value)}
+            {renderDetail('Coords', <a className='underline text-blue-700' href={getGoogleMapsLink()} target='_blank' rel='noreferrer'>{getLatLong()}</a>)}
+        </div>
+    )
+}
